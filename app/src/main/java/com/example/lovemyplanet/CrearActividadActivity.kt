b2d7d40506bc7cc6c249eaf99571018586fdd686
jhemapca <jhemapca@gmail.com>
2021-12-04 04:25:01 -0500

    CasiAcabado

--- app/src/main/java/com/example/lovemyplanet/CrearActividadActivity.kt
+++ app/src/main/java/com/example/lovemyplanet/CrearActividadActivity.kt
@@ -3,10 +3,13 @@ package com.example.lovemyplanet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.*
 import com.example.lovemyplanet.models.ActividadesFullModel
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import java.util.*
 
@@ -14,7 +17,6 @@ class CrearActividadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_actividad)
-
         val spnTipo:Spinner=findViewById(R.id.spnTipoAct)
         val txtMaxAforo:EditText=findViewById(R.id.txtMaxPersonasNewAct)
         val txtFecha:EditText=findViewById(R.id.txtFechaNewAct)
@@ -24,6 +26,11 @@ class CrearActividadActivity : AppCompatActivity() {
         val txtDesc:EditText=findViewById(R.id.txtDescriptionNewAct)
         val btnCrearNuevaActividad:Button=findViewById(R.id.btnSendActividadNueva)
         val db=FirebaseFirestore.getInstance()
+        val dbU = FirebaseAuth.getInstance()
+        val user: FirebaseUser? = dbU.getCurrentUser()
+        val idUser:String = user!!.uid
+        var Ausp:String =""
+
         var spnTipoValue="Limpieza de Playa"
         spnTipo.onItemSelectedListener = object:AdapterView.OnItemSelectedListener{
             override fun onItemSelected(
@@ -41,7 +48,17 @@ class CrearActividadActivity : AppCompatActivity() {
 
         }
 
-
+        db.collection("Auspiciadores").document(idUser).addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w("FIREBASE WARNING", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+            if (snapshot != null && snapshot.exists()) {
+                Ausp=snapshot.data!!["nombre"].toString()
+            }else{
+                Log.d("TAG", "Current data: null")
+            }
+        }
         btnCrearNuevaActividad.setOnClickListener {
             val MaxAforo = txtMaxAforo.text.toString().toInt()
             val Fecha = txtFecha.text.toString()
@@ -57,8 +74,7 @@ class CrearActividadActivity : AppCompatActivity() {
             }else{
                 puntos=4
             }
-
-            val nuevaActi = ActividadesFullModel(spnTipoValue,Fecha, HoraIni, HoraFin, puntos, MaxAforo, Desc, 0, "BASURAMA",Lugar)
+            val nuevaActi = ActividadesFullModel(spnTipoValue,Fecha, HoraIni, HoraFin, puntos, MaxAforo, Desc, 0, Ausp,Lugar)
             val id: UUID = UUID.randomUUID()
             db.collection("actividades").document(id.toString()).set(nuevaActi).addOnSuccessListener{
                 Toast.makeText(this,"Se ha registrado Nueva Actividad",Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/lovemyplanet/ui/fragments/PerfilAuspFragment.kt
+++ app/src/main/java/com/example/lovemyplanet/ui/fragments/PerfilAuspFragment.kt
@@ -1,11 +1,18 @@
 package com.example.lovemyplanet.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.lovemyplanet.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 
 class PerfilAuspFragment : Fragment() {
 
@@ -14,7 +21,65 @@ class PerfilAuspFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil_ausp, container, false)
+        val view= inflater.inflate(R.layout.fragment_perfil_ausp, container, false)
+        val dbU = FirebaseAuth.getInstance()
+        val db= FirebaseFirestore.getInstance()
+        val etNombre:EditText=view.findViewById(R.id.etNombAuspProf)
+        val etEncargado:EditText=view.findViewById(R.id.etEncargadoAuspProf)
+        val etDir:EditText=view.findViewById(R.id.etDirAuspProf)
+        val etPhone:EditText=view.findViewById(R.id.etTelefonoAuspProf)
+        val etCorreo:EditText=view.findViewById(R.id.etCorreoAuspProf)
+        val etPassword:EditText=view.findViewById(R.id.etContraAuspProf)
+        val btnEditProf: FloatingActionButton =view.findViewById(R.id.buttonEditProfAusp)
+        var editing:Boolean = false
+        val user: FirebaseUser? = dbU.getCurrentUser()
+        val idUser:String = user!!.uid
+        db.collection("Auspiciadores").document(idUser).addSnapshotListener{snapshot, e ->
+            if (e != null) {
+                Log.w("FIREBASE WARNING", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+            if (snapshot != null && snapshot.exists()) {
+                etNombre.setText(snapshot.data!!["nombre"].toString())
+                etEncargado.setText(snapshot.data!!["encargado"].toString())
+                etDir.setText(snapshot.data!!["direccion"].toString())
+                etPhone.setText(snapshot.data!!["phone"].toString())
+                etCorreo.setText(snapshot.data!!["correo"].toString())
+                etPassword.setText(snapshot.data!!["contraseña"].toString())
+            }else{
+                Log.d("TAG", "Current data: null")
+            }
+        }
+        btnEditProf.setOnClickListener {
+            if(editing==false){
+                etDir.isEnabled = true
+                etDir.setBackgroundColor(Color.parseColor("#B2EEA2"))
+                etEncargado.isEnabled=true
+                etEncargado.setBackgroundColor(Color.parseColor("#B2EEA2"))
+                etPhone.isEnabled=true
+                etPhone.setBackgroundColor(Color.parseColor("#B2EEA2"))
+                btnEditProf.setImageResource(android.R.drawable.ic_menu_save)
+                editing=true
+            }else{
+                etDir.isEnabled = false
+                etDir.setBackgroundColor(Color.parseColor("#ffffff"))
+                etEncargado.isEnabled=false
+                etEncargado.setBackgroundColor(Color.parseColor("#ffffff"))
+                etPhone.isEnabled=false
+                etPhone.setBackgroundColor(Color.parseColor("#ffffff"))
+                btnEditProf.setImageResource(android.R.drawable.ic_menu_edit)
+                editing=false
+
+                var map:HashMap<String, String> = HashMap()
+                map.put("encargado",etEncargado.text.toString())
+                map.put("direccion",etDir.text.toString())
+                map.put("phone",etPhone.text.toString())
+
+                db.collection("Auspiciadores").document(idUser).update(map as Map<String, Any>)
+            }
+        }
+
+        return view
     }
 
 }
